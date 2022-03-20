6ce8690da95b0bc57a2dec16f84b2367eb65bef9
MFaqihRidho <faqih.fr5@gmail.com>
2022-03-20 13:36:39 +0700

    adding homeand intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Intro from "./components/intro";
+import Home from "./components/home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="flex items-center justify-center font-poppins ">
-            <div className="max-w-[800px] relative min-w-[400px] min-h-screen dark:bg-bg_dark bg-white">
+            <div className="max-w-[800px] relative min-w-[400px] max-h-screen dark:bg-bg_dark bg-white">
                 <Intro></Intro>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                </Routes>
             </div>
         </div>
     );
--- src/components/intro.js
+++ src/components/intro.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
 
 function Intro() {
+    let height = -window.innerHeight;
+
+    const started = () =>
+        gsap.fromTo(
+            "#intro",
+            { y: 0 },
+            { y: height - 200, duration: 4, ease: "circ" }
+        );
+
+    useEffect(() => {}, []);
+
     return (
-        <div className="container absolute flex flex-col items-center justify-center w-full py-10 mx-auto">
+        <div
+            id="intro"
+            className="container absolute z-50 flex flex-col items-center justify-center w-full py-10 mx-auto"
+        >
             <h1 className="mb-3 text-3xl font-bold text-main">Quran Web App</h1>
-            <p className="mb-5 text-lg text-gray-500">
-                Halo Selamat Datang! <br /> Mari Membaca Quran
+            <p className="mb-5 text-lg text-center text-gray-500">
+                Assalamu'alaikum <br /> Mari Membaca Quran
             </p>
             <img
                 className="rounded-3xl w-72"
                 src="https://i.postimg.cc/0258QtgD/Flat-Al-Quran-01-ai-1.png"
                 alt=""
             />
-            <button className="py-3.5 -mt-4 text-lg font-semibold text-white px-7 bg-light_secondary rounded-3xl">
+            <button
+                onClick={() => started()}
+                className="py-3.5 -mt-4 text-lg font-semibold text-white px-7 bg-light_secondary rounded-3xl"
+            >
                 Mulai Membaca
             </button>
         </div>
