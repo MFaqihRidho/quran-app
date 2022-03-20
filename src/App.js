b4593b2d3c02082368bedc82487b681c53f8cf2c
MFaqihRidho <faqih.fr5@gmail.com>
2022-03-20 21:43:24 +0700

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Intro from "./components/intro";
 import Home from "./components/home";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+    const [intro, setIntro] = useState(true);
+
+    useEffect(() => {
+        if (localStorage.getItem("intro") === "false") {
+            setIntro(false);
+        }
+    }, []);
+
     return (
         <div className="flex items-center justify-center font-poppins ">
-            <div className="max-w-[800px] relative min-w-[400px] max-h-screen dark:bg-bg_dark bg-white">
-                <Intro></Intro>
+            <div className="max-w-[800px] w-full container mx-auto px-5 relative min-w-[300px] dark:bg-bg_dark bg-white">
+                {intro && <Intro></Intro>}
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                 </Routes>
--- src/components/intro.js
+++ src/components/intro.js
@@ -4,19 +4,19 @@ import gsap from "gsap";
 function Intro() {
     let height = -window.innerHeight;
 
-    const started = () =>
+    const started = () => {
         gsap.fromTo(
             "#intro",
             { y: 0 },
-            { y: height - 200, duration: 4, ease: "circ" }
+            { y: height - 250, duration: 4, ease: "circ" }
         );
-
-    useEffect(() => {}, []);
+        localStorage.setItem("intro", "false");
+    };
 
     return (
         <div
             id="intro"
-            className="container absolute z-50 flex flex-col items-center justify-center w-full py-10 mx-auto"
+            className="container absolute z-50 flex flex-col items-center justify-center w-full py-10 mx-auto bg-white dark:bg-bg_dark"
         >
             <h1 className="mb-3 text-3xl font-bold text-main">Quran Web App</h1>
             <p className="mb-5 text-lg text-center text-gray-500">
@@ -29,7 +29,7 @@ function Intro() {
             />
             <button
                 onClick={() => started()}
-                className="py-3.5 -mt-4 text-lg font-semibold text-white px-7 bg-light_secondary rounded-3xl"
+                className="py-3.5 -mt-6 text-lg font-semibold text-white px-7 bg-light_secondary rounded-3xl"
             >
                 Mulai Membaca
             </button>
