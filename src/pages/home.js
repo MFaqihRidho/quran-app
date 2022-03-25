59cfa826a95488ed4c353aeaef00137305193fad
MFaqihRidho <faqih.fr5@gmail.com>
2022-03-25 21:05:45 +0700

    deleting search icon

--- src/pages/home.js
+++ src/pages/home.js
@@ -63,22 +63,6 @@ function Home() {
                     <h1 className="text-xl font-bold text-main dark:text-light_secondary">
                         Quran Web App
                     </h1>
-                    <button>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-7 h-7"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                            />
-                        </svg>
-                    </button>
                 </div>
                 <div className="mb-5">
                     <p className="text-lg text-gray-500">Assalamu'alaikum</p>
