15f3e5350c0ac6657213ceb9ef06ee3c317989c1
MFaqihRidho <faqih.fr5@gmail.com>
2022-03-25 21:00:31 +0700

    fixing some error

--- src/pages/bookmark.js
+++ src/pages/bookmark.js
@@ -5,9 +5,7 @@ import gsap from "gsap";
 
 function Bookmark() {
     const navigate = useNavigate();
-    const [local, setLocal] = useState(
-        JSON.parse(localStorage.getItem("bookmark"))
-    );
+    const [local, setLocal] = useState(false);
     const [play, setPlay] = useState(false);
     const [pause, setPause] = useState(false);
     const [id, setId] = useState(false);
@@ -87,7 +85,11 @@ function Bookmark() {
     };
 
     useEffect(() => {
-        setLocal(JSON.parse(localStorage.getItem("bookmark")));
+        if (JSON.parse(localStorage.getItem("bookmark"))) {
+            setLocal(JSON.parse(localStorage.getItem("bookmark")));
+        } else {
+            setLocal(false);
+        }
     }, []);
 
     return (
@@ -99,108 +101,109 @@ function Bookmark() {
                     </h1>
                 </div>
                 <div className="flex flex-col gap-3 pb-7">
-                    {local.map((data) => {
-                        return (
-                            <div>
-                                <div className="flex items-center justify-between w-full px-5 mb-5 rounded-xl h-14 text-main bg-main/10 dark:bg-light_primary/10">
-                                    <p className="px-4 text-lg font-light text-center text-white rounded-full bg-main">
-                                        {`${data.surah} : ${data.data.numberInSurah}`}
-                                    </p>
-                                    <div className="flex gap-5">
-                                        <audio
-                                            id={`audio${data.data.number}`}
-                                            src={data.data.audio}
-                                        ></audio>
-                                        <button
-                                            onClick={() =>
-                                                playAudio(
-                                                    `audio${data.data.number}`
-                                                )
-                                            }
-                                        >
-                                            {pause === false &&
-                                            id ===
-                                                `audio${data?.data?.number}` ? (
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    className="w-10 h-10"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="currentColor"
-                                                    strokeWidth={2}
-                                                >
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                                                    />
-                                                </svg>
-                                            ) : (
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    className="w-10 h-10"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="currentColor"
-                                                    strokeWidth={2}
-                                                >
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                                                    />
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                                    />
-                                                </svg>
-                                            )}
-                                        </button>
-                                        <button
-                                            onClick={() =>
-                                                handleShowModal(data)
-                                            }
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                className="w-10 h-10"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
+                    {local &&
+                        local.map((data) => {
+                            return (
+                                <div>
+                                    <div className="flex items-center justify-between w-full px-5 mb-5 rounded-xl h-14 text-main bg-main/10 dark:bg-light_primary/10">
+                                        <p className="px-4 text-lg font-light text-center text-white rounded-full bg-main">
+                                            {`${data.surah} : ${data.data.numberInSurah}`}
+                                        </p>
+                                        <div className="flex gap-5">
+                                            <audio
+                                                id={`audio${data.data.number}`}
+                                                src={data.data.audio}
+                                            ></audio>
+                                            <button
+                                                onClick={() =>
+                                                    playAudio(
+                                                        `audio${data.data.number}`
+                                                    )
+                                                }
                                             >
-                                                <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
-                                            </svg>
-                                        </button>
-                                        <button
-                                            onClick={() =>
-                                                navigate(
-                                                    `/surah/${data.surah}#${data.data.number}`
-                                                )
-                                            }
-                                            className="px-3 text-white rounded-lg bg-main"
-                                        >
-                                            Go to surah
-                                        </button>
+                                                {pause === false &&
+                                                id ===
+                                                    `audio${data?.data?.number}` ? (
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        className="w-10 h-10"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        stroke="currentColor"
+                                                        strokeWidth={2}
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                                                        />
+                                                    </svg>
+                                                ) : (
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        className="w-10 h-10"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        stroke="currentColor"
+                                                        strokeWidth={2}
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                                                        />
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                                        />
+                                                    </svg>
+                                                )}
+                                            </button>
+                                            <button
+                                                onClick={() =>
+                                                    handleShowModal(data)
+                                                }
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="w-10 h-10"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+                                                </svg>
+                                            </button>
+                                            <button
+                                                onClick={() =>
+                                                    navigate(
+                                                        `/surah/${data.surah}#${data.data.number}`
+                                                    )
+                                                }
+                                                className="px-3 text-white rounded-lg bg-main"
+                                            >
+                                                Go to surah
+                                            </button>
+                                        </div>
                                     </div>
+                                    <div className="flex flex-col gap-4 mb-5">
+                                        <p className="self-end w-full text-4xl text-right">
+                                            {data.data.numberInSurah === 1 &&
+                                            data.surah !== 1
+                                                ? data.data?.text.replace(
+                                                      "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
+                                                      ""
+                                                  )
+                                                : data.data?.text}
+                                        </p>
+                                        <p className="text-gray-500">
+                                            {data.translate}
+                                        </p>
+                                    </div>
+                                    <div className="w-full h-[0.5px] mb-5 bg-gray-500"></div>
                                 </div>
-                                <div className="flex flex-col gap-4 mb-5">
-                                    <p className="self-end w-full text-4xl text-right">
-                                        {data.data.numberInSurah === 1 &&
-                                        data.surah !== 1
-                                            ? data.data?.text.replace(
-                                                  "بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
-                                                  ""
-                                              )
-                                            : data.data?.text}
-                                    </p>
-                                    <p className="text-gray-500">
-                                        {data.translate}
-                                    </p>
-                                </div>
-                                <div className="w-full h-[0.5px] mb-5 bg-gray-500"></div>
-                            </div>
-                        );
-                    })}
+                            );
+                        })}
                 </div>
             </div>
             <Modal
